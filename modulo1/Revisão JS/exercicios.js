aa2f801f119be633babe5aad6e4c0477016fa8c5
unknown <sergiopdjr@hotmail.com>
2022-03-07 10:42:02 -0300

    exercicio 4

--- modulo1/Revisão JS/exercicios.js
+++ modulo1/Revisão JS/exercicios.js
@@ -20,7 +20,13 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-
+    let numerosPares = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            numerosPares.push(array[i])
+        }
+    }
+    return numerosPares
 }
 
 // EXERCÍCIO 05
