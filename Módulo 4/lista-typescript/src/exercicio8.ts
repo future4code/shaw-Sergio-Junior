5e8ea0e363c494d8364f411b0ed1e7b73ec8380f
Sérgio Pereira Dias Júnior <99135090+Sergiopdias@users.noreply.github.com>
2022-05-27 21:52:40 -0300

    correcao ex 8

--- Módulo 4/lista-typescript/src/exercicio8.ts
+++ Módulo 4/lista-typescript/src/exercicio8.ts
@@ -1,21 +1,25 @@
 //-- Exercicio #8
+// const nascimento = prompt("Qual seu ano de nascimento? ")
+// const emissao = prompt("Qual seu ano de emissao? ")
 
-function renovaCarteira(anoAtual: number): boolean {
-    const nascimento = prompt("Qual seu ano de nascimento? formato: 'dd/mm/aaaa' ")
-    const emissao = prompt("Qual seu ano de emissao? formato: 'dd/mm/aaaa' ")
+function renovaCarteira(nascimento: string, emissao: string): boolean {
 
-    const splitNascimento: string[] = nascimento.split("/")
-    const splitEmissao: string[] = emissao.split("/")
+    const anoAtual: number = new Date().getTime()
+    const nascimentoNumber: number = new Date(nascimento).getTime()
+    const emissaoNumber: number = new Date(emissao).getTime()
 
-    const idade: number = anoAtual - Number(splitNascimento[2])
-    const tempoDeCarteira: number = anoAtual - Number(splitEmissao[2])
+    const idade = anoAtual - nascimentoNumber
+    const tempoDeCarteira: number = anoAtual - emissaoNumber
+
+
+
     return (
-        idade <= 20 && tempoDeCarteira >= 5 ? true : false
+        idade <= 630720000000 && tempoDeCarteira >= 157680000000 ? true : false
             ||
-            idade > 20 && tempoDeCarteira >= 10 ? true : false
+            idade > 630720000000 && tempoDeCarteira >= 315360000000 ? true : false
                 ||
-                idade > 50 && tempoDeCarteira >= 15 ? true : false
+                idade > 1576800000000 && tempoDeCarteira >= 473040000000 ? true : false
     )
 }
 
-console.log(renovaCarteira(2022))
+console.log(renovaCarteira("2003/01/01", "2015/01/01"))
