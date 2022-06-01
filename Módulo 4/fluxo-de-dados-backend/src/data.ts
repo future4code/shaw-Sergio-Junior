0bc1eb4ae612e2406cd792e853cfcc0ea184ef26
Sérgio Pereira Dias Júnior <99135090+Sergiopdias@users.noreply.github.com>
2022-06-01 17:28:00 -0300

    fluxo de dados

---
+++ Módulo 4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,19 @@
+type Product = { id: string, name: string, price: number }
+
+export const products: Product[] = [
+    {
+        id: "asc5-asx9",
+        name: "Cenoura",
+        price: 1
+    },
+    {
+        id: "asc2-asx1",
+        name: "Batata",
+        price: 2.5
+    },
+    {
+        id: "asc6-asx3",
+        name: "Cebola",
+        price: 3.5
+    }
+]
