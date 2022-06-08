f47c2bbfa3174f5a3d9a66828773e3f0de29845e
Sérgio Pereira Dias Júnior <99135090+Sergiopdias@users.noreply.github.com>
2022-06-08 13:04:39 -0300

    setando projeto

---
+++ Módulo 4/knex-js/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express } from 'express'
+import cors from 'cors'
+import { connection } from './connection';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+    console.log(`Server in running in http://localhost:3003`)
+})
