2f0c6dc64903a266ac30efbca9bc8d5b3b22eee6
Sérgio Pereira Dias Júnior <99135090+Sergiopdias@users.noreply.github.com>
2022-07-11 19:48:11 -0300

    desafios 3 e 4

--- modulo 5/testes-automatizados/src/index.ts
+++ modulo 5/testes-automatizados/src/index.ts
@@ -1,6 +1,7 @@
 import knex from 'knex'
 import { config } from 'dotenv'
 import { User } from '../model/UserInterface';
+import { Casino, LOCATION, NACIONALITY, Result, ResultItem, UserCasino } from '../model/CasinoInterface';
 
 config()
 
@@ -26,4 +27,44 @@ export function performPurchase(user: User, value: number): User | undefined {
       }
    }
    return undefined
+}
+
+export function verifyAge(casino: Casino, users: UserCasino[]): Result {
+
+   const allowed: UserCasino[] = []
+   const unAllowed: UserCasino[] = []
+
+   for (let user of users) {
+      // br 
+      if (casino.location === LOCATION.BRAZIL) {
+         if (user.age >= 18) {
+            allowed.push(user)
+         } else {
+            unAllowed.push(user)
+         }
+      }
+      // american
+      else if (casino.location === LOCATION.EUA) {
+         if (user.age >= 21) {
+            allowed.push(user)
+         } else {
+            unAllowed.push(user)
+         }
+      }
+   }
+
+   return {
+      brazilians: {
+         allowed: allowed.filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+            .map((user) => user.name),
+         unallowed: unAllowed.filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+            .map((user) => user.name),
+      },
+      americans: {
+         allowed: allowed.filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+            .map((user) => user.name),
+         unallowed: unAllowed.filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+            .map((user) => user.name)
+      }
+   }
 }
--- modulo 5/testes-automatizados/tests/performPurchase.test.ts
+++ modulo 5/testes-automatizados/tests/performPurchase.test.ts
@@ -1,5 +1,6 @@
+import { Casino, LOCATION, NACIONALITY, UserCasino } from "../model/CasinoInterface";
 import { User } from "../model/UserInterface"
-import { performPurchase } from "../src"
+import { performPurchase, verifyAge } from "../src"
 
 describe("Testing function performPurchase", () => {
 
@@ -42,4 +43,132 @@ describe("Testing function performPurchase", () => {
         expect(result).not.toBeDefined()
     })
 
+});
+
+describe("Testing function verifyAge", () => {
+
+    test("Testing brazilian in Brazil.", () => {
+        const casino: Casino = {
+            name: "Conrado",
+            location: LOCATION.BRAZIL
+        }
+
+        const user: UserCasino = {
+            name: "Joao",
+            age: 18,
+            nacionality: NACIONALITY.BRAZILIAN
+        }
+
+        const result = verifyAge(casino, [user])
+
+        expect(result).toEqual({ "americans": { "allowed": [], "unallowed": [] }, "brazilians": { "allowed": [user.name], "unallowed": [] } })
+    })
+
+    test("Testing american in Brazil.", () => {
+        const casino: Casino = {
+            name: "Conrad",
+            location: LOCATION.BRAZIL
+        }
+
+        const user: UserCasino = {
+            name: "John",
+            age: 20,
+            nacionality: NACIONALITY.AMERICAN
+        }
+
+        const result = verifyAge(casino, [user])
+
+        expect(result).toEqual({ "americans": { "allowed": [user.name], "unallowed": [] }, "brazilians": { "allowed": [], "unallowed": [] } })
+    })
+
+    test("Testing brazilians and americans aged 19yrs old in USA.", () => {
+        const casino: Casino = {
+            name: "Peter Crown",
+            location: LOCATION.EUA
+        }
+
+        const user: UserCasino[] = [
+            {
+                name: "Maria",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Manuela",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Mary",
+                age: 19,
+                nacionality: NACIONALITY.AMERICAN
+            },
+            {
+                name: "Jonny",
+                age: 19,
+                nacionality: NACIONALITY.AMERICAN
+            }
+        ]
+
+        const result = verifyAge(casino, user)
+
+        expect(result).toEqual({
+            "americans": { "allowed": [], "unallowed": ["Mary", "Jonny"] },
+            "brazilians": { "allowed": [], "unallowed": ["Maria", "Manuela"] }
+        })
+    })
+
+    test("Testing brazilians (19yrs) and americans (21yrs old) in USA.", () => {
+        const casino: Casino = {
+            name: "Peter Crown",
+            location: LOCATION.EUA
+        }
+
+        const user: UserCasino[] = [
+            {
+                name: "Maria",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Manuela",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Mary",
+                age: 21,
+                nacionality: NACIONALITY.AMERICAN
+            },
+            {
+                name: "Jonny",
+                age: 21,
+                nacionality: NACIONALITY.AMERICAN
+            }
+        ]
+
+        const result = verifyAge(casino, user)
+
+        expect(result).toEqual({
+            "americans": { "allowed": ["Mary", "Jonny"], "unallowed": [] },
+            "brazilians": { "allowed": [], "unallowed": ["Maria", "Manuela"] }
+        })
+    })
+
+    // test("Testing users ages.", () => {
+    //     const casino: Casino = {
+    //         name: "Conrad",
+    //         location: LOCATION.EUA
+    //     }
+
+    //     const user: UserCasino = {
+    //         name: "Maria",
+    //         age: 25,
+    //         nacionality: NACIONALITY.BRAZILIAN
+    //     }
+
+    //     const result = verifyAge(casino, [user])
+
+    //     expect(result).toEqual({ "americans": { "allowed": [], "unallowed": [] }, "brazilians": { "allowed": [user.name], "unallowed": [] } })
+    // })
 })
